0dc2d91bd3f2577feecf2778f441f139c2734077
Guzman Hernandez <guzmanhernandez90@gmail.com>
2020-06-03 17:13:15 +0100

    Init  repository

---
+++ EOI-FULLSTACK-EJ-01-master/EOI-FULLSTACK-EJ-01-master/js/main.js
@@ -0,0 +1,30 @@
+
+
+// Devuelve EL NOMBRE del mayor de los tres
+function quienEsMayorDeLosTres(p1, p2, p3){
+  console.log(typeof p1.age)
+  console.log(typeof p2.age)
+  console.log(typeof p3.age)
+  if ((p1.age > p2.age)  &&  (p1.age > p3.age ))  {
+    console.log("P1 es mayor")
+  }
+
+}
+
+// Devuelve en forma de boolean el resultado
+// NOTA: las mayúsculas y minúsculas no se consideran distintas
+function hayAlgunCorreoRepetido(p1, p2, p3){
+}
+
+// Pregunta al usuario por su edad y devuelve en boolean la respuesta
+function elUsuarioEsMayorQueLosTres(p1, p2, p3){
+}
+
+// Devuelve el número de los que tienen correo acabado en gmail.com
+// RESTRICCIÓN: Resuélvelo usando los métodos .indexOf() ó .lastIndexOf()
+// RESTRICCIÓN: Resuélvelo usando el método .search()
+// RESTRICCIÓN: Resuélvelo usando los métodos .substring() ó substr()
+// RESTRICCIÓN: Plantea otra forma de solucionarlo que no use ninguno de los métodos de arriba
+// Deja descomentada solo una de las soluciones
+function cuantosTienenCorreoDeGmailDotCom(p1, p2, p3){
+}
