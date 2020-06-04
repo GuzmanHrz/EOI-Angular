8ec77793516d2807eb239387f39ca0d1d6de2406
Guzman Hernandez <guzmanhernandez90@gmail.com>
2020-06-04 20:25:45 +0100

    Add solutions to EJ-01

--- EOI-FULLSTACK-EJ-01-master/js/main.js
+++ EOI-FULLSTACK-EJ-01-master/js/main.js
@@ -2,23 +2,47 @@
 
 // Devuelve EL NOMBRE del mayor de los tres
 function quienEsMayorDeLosTres(p1, p2, p3){
-  console.log(typeof p1.age)
-  console.log(typeof p2.age)
-  console.log(typeof p3.age)
-  if ((p1.age > p2.age)  &&  (p1.age > p3.age ))  {
-    console.log("P1 es mayor")
-  }
+  let copyP1 = {name: p1.name, age: Number(p1.age)};
+  let copyP2 = {name: p2.name, age: Number(p2.age)};
+  let copyP3 = {name: p3.name, age: Number(p3.age)};
 
+  if ((copyP1.age > copyP2.age) && (copyP1.age > copyP3.age)) {
+    return  copyP1.name;
+  }else if ((copyP2.age > copyP1.age) && (copyP2.age > copyP3.age)) {
+    return  copyP2.name;
+  }else if ((copyP3.age > copyP2.age) && (copyP3.age > copyP1.age)) {
+    return  copyP3.name;    
+  }else {
+    return "ninguno, todos tienen la misma edad";
+  }
 }
 
 // Devuelve en forma de boolean el resultado
 // NOTA: las mayúsculas y minúsculas no se consideran distintas
 function hayAlgunCorreoRepetido(p1, p2, p3){
+  let emailP1 = p1.email.toLowerCase();
+  let emailP2 = p2.email.toLowerCase();
+  let emailP3 = p3.email.toLowerCase();
+  if ((emailP1 === emailP2) || (emailP1 === emailP3) || (emailP2 === emailP3)){
+    return true;
+  }
 }
+
 
 // Pregunta al usuario por su edad y devuelve en boolean la respuesta
 function elUsuarioEsMayorQueLosTres(p1, p2, p3){
-}
+  
+  let userAge = prompt("Introduce tu edad");
+  let AgeP1 = Number(p1.age);
+  let AgeP2 = Number(p2.age);
+  let AgeP3 = Number(p3.age);
+
+  if  ((userAge > AgeP1) && (userAge > AgeP2) && (userAge > AgeP3)){
+    return  true;
+  } else {
+    return false;
+  }
+} 
 
 // Devuelve el número de los que tienen correo acabado en gmail.com
 // RESTRICCIÓN: Resuélvelo usando los métodos .indexOf() ó .lastIndexOf()
@@ -26,5 +50,35 @@ function elUsuarioEsMayorQueLosTres(p1, p2, p3){
 // RESTRICCIÓN: Resuélvelo usando los métodos .substring() ó substr()
 // RESTRICCIÓN: Plantea otra forma de solucionarlo que no use ninguno de los métodos de arriba
 // Deja descomentada solo una de las soluciones
+
 function cuantosTienenCorreoDeGmailDotCom(p1, p2, p3){
+  let emailList = [p1.email.trim(), p2.email.trim(), p3.email.trim()];
+  let gmail = "@gmail.com";
+  var numberOfEmails = 0;
+
+  emailList.forEach ( function (email) {
+
+    // Metodo .lastIndexOf()
+    /* if ((email.lastIndexOf(gmail) + gmail.length )  ==  email.length){
+        ++numberOfEmails;
+    } */
+
+    //Metodo .search()
+    /* if ( email.search(/@gmail.com$/) !== -1 ){
+        ++numberOfEmails;
+    } */
+
+    //Metodo .substr()
+    /* if (email.substr(email.indexOf('@')) === gmail){
+        ++numberOfEmails;
+     } */
+
+    //Metodo .slice()
+    if (email.slice(-gmail.length) === gmail ){
+      ++numberOfEmails;
+    }
+  });
+
+  return numberOfEmails;
+  
 }
